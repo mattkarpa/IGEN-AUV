92819af0fc38f1a6c2bcf8fa3652b25137c3a92c
mattkarpa <mattkarpa+github@gmail.com>
2012-04-01 13:53:47 -0700

    Commented.

--- FullTest/FullTest.ino
+++ FullTest/FullTest.ino
@@ -6,6 +6,7 @@ void setup() {
 }
 
 void loop() {
+  //Move forwards for 15s
   digitalWrite(3,HIGH);
   digitalWrite(2,HIGH);
   delay(15000);
@@ -13,11 +14,13 @@ void loop() {
   digitalWrite(2,LOW);
   delay(5000);
 
+  //Turn for 5s
   digitalWrite(3,HIGH);
   delay(5000);
   digitalWrite(3,LOW);
   delay(5000);
 
+  //Move up for 5s
   digitalWrite(4,HIGH);
   delay(5000);
   digitalWrite(4,LOW);
